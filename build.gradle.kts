440c4498d612603152f7fcc818f9fe205a37a2a1
Claus Holst <clh@visiolink.com>
2017-10-30 18:37:40 +0100

    Update Gradle wrapper version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("jvm", "1.1.51")
+    kotlin("jvm") version "1.1.51"
     `maven-publish`
     `java-gradle-plugin`
 }
 
 dependencies {
     compileOnly(gradleApi())
-    implementation("com.android.tools.build:gradle:3.0.0-beta7")
+    implementation("com.android.tools.build:gradle:3.0.0")
     implementation(kotlin("stdlib", "1.1.51"))
     implementation("org.json:json:20160810")
 }
